16cd57cac48c343a645c32922c3de668d45296bf
Subrata Das <40207448+subratappt@users.noreply.github.com>
2021-09-06 13:39:53 +0530

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -70,16 +70,16 @@ telescope.setup(
     }
 )
 
-local extensions = { "fzf" }
-local packer_repos = [["extensions", "telescope-fzf-native.nvim"]]
+-- local extensions = { "fzf" }
+-- local packer_repos = [["extensions", "telescope-fzf-native.nvim"]]
 
-if vim.fn.executable "ueberzug" == 1 then
-   table.insert(extensions, "media_files")
-   packer_repos = packer_repos .. ', "telescope-media-files.nvim"'
-end
+-- if vim.fn.executable "ueberzug" == 1 then
+--    table.insert(extensions, "media_files")
+--    packer_repos = packer_repos .. ', "telescope-media-files.nvim"'
+-- end
 
    
-for _, ext in ipairs(extensions) do
-      telescope.load_extension(ext)
-end
+-- for _, ext in ipairs(extensions) do
+--       telescope.load_extension(ext)
+-- end
  
