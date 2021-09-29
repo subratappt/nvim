0dc7a587e87c98b9a57ff63ea9d143b6bfad283c
Subrata Das <40207448+subratappt@users.noreply.github.com>
2021-09-29 19:13:19 +0530

    Update nvimtree.lua

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,6 +1,8 @@
 local opts = {silent = true, noremap = true}
-vim.api.nvim_set_keymap('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>NvimTreeRefresh<CR>', opts)
+-- vim.api.nvim_set_keymap('n', '<C-n>', '<Cmd>NvimTreeToggle<CR>', opts)
+-- vim.api.nvim_set_keymap('n', '<leader>r', '<Cmd>NvimTreeRefresh<CR>', opts)
+nnoremap <C-n> :NvimTreeToggle<CR>
+nnoremap <leader>r :NvimTreeRefresh<CR>
 
 local ok, tree_c = pcall(require, 'nvim-tree.config')
 if not ok then
