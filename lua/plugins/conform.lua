7e904f4b37aaf19d1efce966360ef3bfb657f1a2
Subrata Das <subratappt@gmail.com>
2024-07-25 18:32:03 -0400

    update

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,76 +1,69 @@
-vim.api.nvim_create_user_command("FormatDisable", function(args)
-    if args.bang then
-        -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
-    else
-        vim.g.disable_autoformat = true
-    end
-end, {
-    desc = "Disable autoformat-on-save",
-    bang = true
-})
-
-vim.api.nvim_create_user_command("FormatEnable", function()
-    vim.b.disable_autoformat = false
-    vim.g.disable_autoformat = false
-end, {
-    desc = "Re-enable autoformat-on-save"
-})
-
-local options = {
-    lsp_fallback = true,
-
-    formatters_by_ft = {
-        lua = {
-            "stylua"
-        },
-
-        sh = {
-            "shfmt"
-        },
-
-        cpp = {
-            "clang-format"
-        },
-
-        c = {
-            "clang-format"
-        },
-
-        python = {
-            "black"
-        }
-    },
-
-    format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-            return
-        end
-        return {
-            timeout_ms = 3000,
-            lsp_fallback = true
-        }
-    end
-}
-
 return {
-    "stevearc/conform.nvim",
-    --  for users those who want auto-save conform + lazyloading!
-    event = "BufWritePre",
-    cmd = {
-        "ConformInfo"
-    },
-    config = function()
-        require("conform").setup(options)
-    end,
+	"stevearc/conform.nvim",
+	event = {
+		"BufWritePre",
+	},
+	cmd = {
+		"ConformInfo",
+	},
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>fm",
+			function()
+				require("conform").format({
+					async = true,
+				})
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- This will provide type hinting with LuaLS
+	---@module "conform"
+	---@type conform.setupOpts
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = {
+				"stylua",
+				config = "~/.config/nvim/lua/stylua.toml",
+			},
+			python = {
+				-- "isort",
+				"black",
+			},
+			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			sh = {
+				"shfmt",
+			},
 
-    -- Keymap
-    vim.keymap.set("n", "<leader>fm", function()
-        vim.lsp.buf.format {
-            async = true
-        }
-    end, {
-        desc = "Formating",
-        silent = true
-    })
+			cpp = {
+				"clang-format",
+			},
+
+			c = {
+				"clang-format",
+			},
+		},
+		-- Set default options
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		-- Set up format-on-save
+		format_on_save = {
+			timeout_ms = 500,
+		},
+		-- Customize formatters
+		--   formatters = {
+		--     shfmt = {
+		--       prepend_args = { "-i", "2" },
+		--     },
+		--   },
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr =
+			"v:lua.require'conform'.formatexpr()"
+	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -13,160 +13,160 @@ local colors = {
 }
 
 local bubbles_theme = {
-    normal = {
-        a = {
-            fg = colors.black,
-            bg = colors.violet
-        },
-        b = {
-            fg = colors.white,
-            bg = colors.grey
-        },
-        c = {
-            fg = colors.white
-        }
-    },
+	normal = {
+		a = {
+			fg = colors.black,
+			bg = colors.violet,
+		},
+		b = {
+			fg = colors.white,
+			bg = colors.grey,
+		},
+		c = {
+			fg = colors.white,
+		},
+	},
 
-    insert = {
-        a = {
-            fg = colors.black,
-            bg = colors.blue
-        }
-    },
-    visual = {
-        a = {
-            fg = colors.black,
-            bg = colors.cyan
-        }
-    },
-    replace = {
-        a = {
-            fg = colors.black,
-            bg = colors.red
-        }
-    },
+	insert = {
+		a = {
+			fg = colors.black,
+			bg = colors.blue,
+		},
+	},
+	visual = {
+		a = {
+			fg = colors.black,
+			bg = colors.cyan,
+		},
+	},
+	replace = {
+		a = {
+			fg = colors.black,
+			bg = colors.red,
+		},
+	},
 
-    inactive = {
-        a = {
-            fg = colors.white,
-            bg = colors.black
-        },
-        b = {
-            fg = colors.white,
-            bg = colors.black
-        },
-        c = {
-            fg = colors.white
-        }
-    }
+	inactive = {
+		a = {
+			fg = colors.white,
+			bg = colors.black,
+		},
+		b = {
+			fg = colors.white,
+			bg = colors.black,
+		},
+		c = {
+			fg = colors.white,
+		},
+	},
 }
 
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    config = function()
-        require("lualine").setup({
-            options = {
-                theme = bubbles_theme,
-                component_separators = {
-                    left = "",
-                    right = ""
-                },
-                section_separators = {
-                    left = "",
-                    right = ""
-                },
-                -- section_separators = { left = "", right = "" },
-                icons_enabled = true,
-                disabled_filetypes = {
-                    "neo-tree",
-                    "NvimTree",
-                    "Outline",
-                    "packer"
-                }
-            },
-            sections = {
-                lualine_a = {
-                    {
-                        "mode",
-                        separator = {
-                            left = "",
-                            right = ""
-                        },
-                        padding = {
-                            left = 0,
-                            right = 0
-                        }
-                    }
-                },
-                lualine_b = {
-                    {
-                        "filename",
-                        path = 1
-                    },
-                    "branch"
-                },
-                lualine_c = {
-                    {
-                        "diagnostics",
-                        sources = {
-                            "nvim_lsp"
-                        },
-                        symbols = {
-                            error = " ",
-                            warn = " ",
-                            info = " "
-                        },
-                        color_error = colors.red,
-                        color_warn = colors.violet,
-                        color_info = colors.blue
-                    }
-                },
-                lualine_x = {},
-                lualine_y = {
-                    "filetype",
-                    "progress"
-                },
-                lualine_z = {
-                    {
-                        "location",
-                        separator = {
-                            right = ""
-                        }
-                    }
-                }
-            },
-            inactive_sections = {
-                lualine_a = {
-                    {
-                        "filename",
-                        separator = {
-                            left = "",
-                            right = ""
-                        }
-                    }
-                },
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {
-                    {
-                        "location",
-                        separator = {
-                            left = "",
-                            right = ""
-                        }
-                    }
-                }
-            },
-            tabline = {},
-            extensions = {}
-        })
-        vim.cmd("highlight statusline guifg=white guibg=NONE gui=NONE")
-        vim.cmd("highlight statuslineNC guifg=black guibg=NONE gui=NONE")
-    end
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = bubbles_theme,
+				component_separators = {
+					left = "",
+					right = "",
+				},
+				section_separators = {
+					left = "",
+					right = "",
+				},
+				-- section_separators = { left = "", right = "" },
+				icons_enabled = true,
+				disabled_filetypes = {
+					"neo-tree",
+					"NvimTree",
+					"Outline",
+					"packer",
+				},
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						separator = {
+							left = "",
+							right = "",
+						},
+						padding = {
+							left = 0,
+							right = 0,
+						},
+					},
+				},
+				lualine_b = {
+					{
+						"filename",
+						path = 1,
+					},
+					"branch",
+				},
+				lualine_c = {
+					{
+						"diagnostics",
+						sources = {
+							"nvim_lsp",
+						},
+						symbols = {
+							error = " ",
+							warn = " ",
+							info = " ",
+						},
+						color_error = colors.red,
+						color_warn = colors.violet,
+						color_info = colors.blue,
+					},
+				},
+				lualine_x = {},
+				lualine_y = {
+					"filetype",
+					"progress",
+				},
+				lualine_z = {
+					{
+						"location",
+						separator = {
+							right = "",
+						},
+					},
+				},
+			},
+			inactive_sections = {
+				lualine_a = {
+					{
+						"filename",
+						separator = {
+							left = "",
+							right = "",
+						},
+					},
+				},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {
+					{
+						"location",
+						separator = {
+							left = "",
+							right = "",
+						},
+					},
+				},
+			},
+			tabline = {},
+			extensions = {},
+		})
+		vim.cmd("highlight statusline guifg=white guibg=NONE gui=NONE")
+		vim.cmd("highlight statuslineNC guifg=black guibg=NONE gui=NONE")
+	end,
 }
